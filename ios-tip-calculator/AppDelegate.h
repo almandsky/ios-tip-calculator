0c8c805c6a4e2a25e3e717d052b3e292a6fa1d32
Sky Chen <skychen@yahoo-inc.com>
2016-09-08 15:42:23 -0700

    init commit

---
+++ ios-tip-calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-tip-calculator
+//
+//  Created by Sky Chen on 9/8/16.
+//  Copyright © 2016 Sky Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-tip-calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-tip-calculator
+//
+//  Created by Sky Chen on 9/8/16.
+//  Copyright © 2016 Sky Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
