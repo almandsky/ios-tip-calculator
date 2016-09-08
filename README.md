ed5846841fe0fdba371e1ced9741f5894db58799
Sky Chen <skychen@yahoo-inc.com>
2016-09-08 16:14:14 -0700

    first complated version

---
+++ README.md
@@ -0,0 +1,3 @@
+#iOS Tip Calculator
+
+
--- ios-tip-calculator/ViewController.m
+++ ios-tip-calculator/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billTextValue;
+@property (weak, nonatomic) IBOutlet UILabel *tipLable;
+@property (weak, nonatomic) IBOutlet UILabel *totalLable;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
 
 @implementation ViewController
@@ -18,11 +23,32 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.title = @"Tip Calculator";
+    // self.tipLable.text = @"$5.00";
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)onTap:(UITapGestureRecognizer *)sender {
+    [self.view endEditing:YES];
+    [self updateValues];
+}
+- (IBAction)onValueChange:(UISegmentedControl *)sender {
+    [self updateValues];
+}
+
+- (void)updateValues {
+    // get the bill amount
+    float billAmount = [self.billTextValue.text floatValue];
+    // compute the tip and total
+    NSArray *tipValues = @[@(0.15), @(0.18), @(0.2)];
+    float tipAmont = [tipValues[self.tipControl.selectedSegmentIndex] floatValue] * billAmount;
+    float totalAmount = billAmount + tipAmont;
+    // update UI
+    self.tipLable.text = [NSString stringWithFormat:@"$%0.2f", tipAmont];
+    self.totalLable.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+
 }
 
 @end
